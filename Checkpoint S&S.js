649e5e79b6104eeb22a48c130fbfeacc6b148c82
syrinejoulak <73502225+syrinejoulak@users.noreply.github.com>
2021-02-24 18:07:29 +0100

    Update Checkpoint S&S.js

--- Checkpoint S&S.js
+++ Checkpoint S&S.js
@@ -1,3 +1,14 @@
 function rearrange(cards) {
 return cards.sort(function(a, b){return a-b});
 }
+
+function rearrange(cards) {
+for (var i = 1; i < cards.length; i++)
+    for (var j = 0; j < i; j++)
+        if (cards[i] < cards[j]) {
+          var x = cards[i];
+          cards[i] = cards[j];
+          cards[j] = x;
+        }
+  return cards
+}
